49b7a1bc0ccccf8f3a236d0f458c22d11bd90b75
nicolaslema <nicolemadev@gmail.com>
2024-04-21 06:35:29 -0300

    Added API

--- src/components/CardPLayButton.jsx
+++
@@ -1,6 +0,0 @@
-import { Pause, Play } from "./Player"
-export function CardPlayButton({ id }) {
-    return (
-        <div className="card-play-button rounded-full bg-green-500 p-2"><Play/></div>
-    )
-}
---
+++ src/pages/api/get-info-playlist.json.js
@@ -0,0 +1,7 @@
+import { allPlaylists, songs } from "../../lib/data"
+
+
+export async function GET({ params, request }) {
+
+    
+}
