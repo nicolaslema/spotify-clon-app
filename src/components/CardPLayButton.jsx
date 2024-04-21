a2d55e610721ecff1c99cdfda323d2643208e4c7
nicolaslema <nicolemadev@gmail.com>
2024-04-21 05:52:24 -0300

    Added Transitions & Fix audio persistance.

---
+++ src/components/CardPLayButton.jsx
@@ -0,0 +1,6 @@
+import { Pause, Play } from "./Player"
+export function CardPlayButton({ id }) {
+    return (
+        <div className="card-play-button rounded-full bg-green-500 p-2"><Play/></div>
+    )
+}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Pause = ({ className }) => (
   <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -16,25 +16,24 @@ export const Volume = () => (
   <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
 )
   
-
 export function Player() {
     const [isPlaying, setIsPlaying] = useState(false)
     const [currentSong, setCurrentSong] = useState(null)
     const audioRef = useRef()
 
+    useEffect(() => {
+        audioRef.current.src = `/music/1/01.mp3`
+    },
+        [])
+
     const handleClick = () => {
         if (isPlaying) {
             audioRef.current.pause()
         } else {
-            audioRef.current.src = `/music/1/01.mp3`
-            audioRef.current.play()
-            
-            
+            audioRef.current.play() 
         }
         setIsPlaying(!isPlaying)
-        
     }
-
 
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
