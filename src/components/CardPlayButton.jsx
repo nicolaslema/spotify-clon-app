69776239e297d311ac16406f137c8c2a408c2164
nicolaslema <nicolemadev@gmail.com>
2024-04-23 16:29:42 -0300

    Added: Volume Slider, Reproduce different tracks

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,35 +1,34 @@
-import { Pause, Play } from "./Player"
-import { usePlayerStore } from "../store/playerStore"
+import { Pause, Play } from "./Player";
+import { usePlayerStore } from "../store/playerStore";
 
 export function CardPlayButton({ id }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
+    usePlayerStore((state) => state);
 
-    const {
-    currentMusic,
-    isPlaying,
-    setIsPlaying,
-    setCurrentMusic
-    } = usePlayerStore(state => state)
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
-    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
-
-    const handleClick = () => {
-
-        if (isPlayingPlaylist) {
-            setIsPlaying(false)
-            return
-        }
-
-        fetch(`/api/get-info-playlist.json?id=${id}`).then(res => res.json()).then(data => {
-            const { song, playlist } = data
-            setIsPlaying(true)
-            setCurrentMusic({songs, playlist, song: song[0]})
-        })
-      
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return;
     }
 
-    return (
-        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-2">
-            {isPlayingPlaylist? <Pause/> : <Play/>}
-        </button>
-    )
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { songs, playlist } = data;
+        setIsPlaying(true);
+        setCurrentMusic({ songs, playlist, song: songs[0] });
+      });
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="card-play-button rounded-full bg-green-500 p-2"
+    >
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  );
 }
+
